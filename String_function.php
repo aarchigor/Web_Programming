2673f34cb1c2a733fd79fb3010a006c00b1ff49d
aarchigor <154911691+aarchigor@users.noreply.github.com>
2024-01-11 08:41:23 +0530

    Add files via upload

---
+++ String_function.php
@@ -0,0 +1,25 @@
+<?php
+
+
+$a=strlen("Atmiya");
+$b=strpos("atmiy ,uni","hello");
+$c=str_replace("atmiya","uni","hello");
+$d=substr("atmiya",0,2);
+$e=strtolower("ATMIYA");
+$f=strtoupper("atmiya");
+$g=ucfirst("atmiya");
+$h=strrev("atmiya");
+$i=trim("atmiya, uni!");
+
+echo $a;
+echo "<br>";
+echo $b;
+echo "<br>";
+echo $c ."<br>";
+echo $d ."<br>";
+echo $e ."<br>";
+echo $f ."<br>";
+echo $g ."<br>";
+echo $h ."<br>";
+echo $i ."<br>";
+?>
