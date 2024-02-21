978a3a31d373a545f2712e70e91a2caf80c1796f
aarchigor <154911691+aarchigor@users.noreply.github.com>
2024-02-21 23:05:51 +0530

    Add files via upload

---
+++ Table/table_mul.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<form action="multi_table.php" method="get">
+Enter start value<input type="number" name="sno">
+Enter last value<input type="number" name="lno">
+<input type="submit" name="submit">
+</form>
+
+</body>
+</html>
+<?php
+?>
